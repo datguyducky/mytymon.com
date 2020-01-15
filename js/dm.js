fa858d2c0a0590b9e1e323b0d283a5c4e76bcd04
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2020-01-15 13:53:49 +0100

    fixed dark mode for index.html

--- js/dm.js
+++ js/dm.js
@@ -1,31 +1,44 @@
 //function to save 'mode' to storage.
 function darkMode() {
-    currentMode = localStorage.getItem("mode");
-    if(currentMode == "dark"){localStorage.setItem("mode", "nodark");}
-    else{localStorage.setItem("mode", "dark");}
+    let currentMode = localStorage.getItem('mode');
+    if(currentMode == 'dark'){localStorage.setItem('mode', 'nodark');}
+    else{localStorage.setItem('mode', 'dark');}
     location.reload(false)
 }
 
 //on page load getting 'mode' from local storage -> and if 'mode' is set to 'dark' then page style is set to dark mode.
 window.onload = function() {
-    var mode = localStorage.getItem("mode");
-    var body = document.getElementById('body');
-    var btn = document.getElementById('night');
-    var header = document.getElementById('me');
+    let mode = localStorage.getItem('mode');
+    let body = document.getElementById('body');
+    let btn = document.getElementById('night');
+	let header = document.getElementById('me');
+	let contact_a = document.getElementsByClassName('contact-a');
+	let github = document.getElementsByClassName('feather-github');
+	let s_card = document.getElementsByClassName('s-card');
 
     if (mode == 'dark'){
         body.style.backgroundColor = '#222';
         body.style.color = '#f3f3f3';
         
         btn.style.backgroundColor = '#f3f3f3';
-        btn.innerHTML = '🌑';
+		btn.innerHTML = '🌑';
+		
+		for(let i=0; i<contact_a.length; i++) {
+			contact_a[i].style.color = "#bbb";
+		}
+		github[0].style.color='#f3f3f3'
+
+		for(let i=0; i<s_card.length; i++) {
+			s_card[i].style.backgroundColor = '#444';
+			s_card[i].style.borderColor = '#444';
+		}
         
         header.style.color = '#bbb';
 
-        localStorage.setItem("mode", "dark");
+        localStorage.setItem('mode', 'dark');
     }
 
     else {
-        console.log("dark mode is off");
+        console.log('dark mode is off');
     }
 }
