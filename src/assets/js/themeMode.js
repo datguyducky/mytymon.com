ab3b42a6b1b28d56c3fff47d01549b3bf9d0db7b
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2023-06-07 21:49:02 +0200

    feat: add duck images, display and hide correct images on theme button

--- src/assets/js/themeMode.js
+++ src/assets/js/themeMode.js
@@ -1,11 +1,17 @@
 const changeThemeToDark = () => {
 	document.documentElement.setAttribute('data-theme', 'dark'); // set theme to dark
 	localStorage.setItem('data-theme', 'dark'); // save theme to local storage
+
+	// hiding and displaying correct image for change theme button
+	displayThemeImage('dark');
 };
 
 const changeThemeToLight = () => {
 	document.documentElement.setAttribute('data-theme', 'light'); // set theme light
 	localStorage.setItem('data-theme', 'light'); // save theme to local storage
+
+	// hiding and displaying correct image for change theme button
+	displayThemeImage('light');
 };
 
 // Get the element based on ID
@@ -28,3 +34,13 @@ if (theme === 'dark') {
 } else {
 	document.documentElement.setAttribute('data-theme', 'light');
 }
+
+const displayThemeImage = newTheme => {
+	if (newTheme === 'dark') {
+		document.getElementById('duck-sleepy').style.display = 'none';
+		document.getElementById('duck-sunny').style.display = 'inline-block';
+	} else {
+		document.getElementById('duck-sunny').style.display = 'none';
+		document.getElementById('duck-sleepy').style.display = 'inline-block';
+	}
+};
